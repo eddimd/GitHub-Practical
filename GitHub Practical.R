2823de8130630ec6a82e01da7da263cfa538b509
eddimd <s1933945@ed.ac.uk>
2025-09-24 14:27:13 +0100

    I added a comment

--- GitHub Practical.R
+++ GitHub Practical.R
@@ -15,4 +15,4 @@ summary(model1)
 ggplot(penguins, aes(x=flipper_length_mm, y= body_mass_g, colour= species)) + geom_point()+stat_smooth(method="lm")
 ggsave("figs/1_flipper_bodymass_regression.png")
 
-
+#this is a comment
